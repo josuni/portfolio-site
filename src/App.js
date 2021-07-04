181ecdd96e4b9ccb414ade103ec8bdc2c1ee2b33
Jo <sjjjcontact@gmail.com>
2021-07-04 01:06:09 -0400

    Changed text on card

--- src/App.js
+++ src/App.js
@@ -12,14 +12,15 @@ function App() {
       </h2>
     </Navbar>
       <div style={{display: 'flex', justifyContent: 'center'}}>
-      <Card style={{ width: '18rem' }}>
+      <Card border="danger" style={{ width: '20rem'}}>
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>Hi there!</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
+            <p>
+            This website is a work in progress. <br></br>
+            I'm making a portfolio.
+            </p>
           </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
       </div>
